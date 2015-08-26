2d5cf4497c4a66e13b314a955be0640807b34be0
omaracrystal <omaracrystal@gmail.com>
2015-08-26 15:59:13 -0600

    added a function solution to problem

--- reverse_words.js
+++ reverse_words.js
@@ -1,15 +1,11 @@
+$(document).ready(function() {
+  console.log("The document is ready!");
+});
+
 var message = 'find you will pain only go you recordings security the into if'
 
 var results = 'if into the security recordings you go only pain will you find'
 
 function reverseMessage(message) {
   return message.split(" ").reverse().join(" ");
-}
-
-function reverseMessage2(message) {
-  var messageArray = message.split(" ");
-
-  for (var i = 0; i < messageArray.length; i++) {
-    messageArray[i]
-  };
 }
