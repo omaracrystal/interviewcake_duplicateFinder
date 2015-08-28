56874ae3b7cf6bb7587128bd6a450fd816a221e8
omaracrystal <omaracrystal@gmail.com>
2015-08-27 23:39:35 -0600

    added two more solutions

--- reverse_words.js
+++ reverse_words.js
@@ -1,11 +1,63 @@
-$(document).ready(function() {
-  console.log("The document is ready!");
-});
-
 var message = 'find you will pain only go you recordings security the into if'
 
 var results = 'if into the security recordings you go only pain will you find'
 
-function reverseMessage(message) {
-  return message.split(" ").reverse().join(" ");
+///////////////////////////////////* RESULT 1 *////////////////////////////////////
+function reverseMessage1(string) {
+  var array = string.split(" ");
+  var result="";
+  for (var i = array.length-1; i >=0; i--) {
+    result+=array[i]+" ";
+  }
+  console.log(result);
+};
+
+reverseMessage1(message);
+
+
+///////////////////////////////////* RESULT 2 *////////////////////////////////////
+function reverseMessage2(string){
+  var array = string.split(/ /),
+    length = array.length;
+    i = length/2 | 0;
+  while (i--) array.splice(i, 1, array.splice(length-1-i, 1, array[i])[0]);
+  return array.join(' ');
 }
+
+reverseMessage2(message);
+
+
+///////////////////////////////////* RESULT 3 *////////////////////////////////////
+function reverseMessage3(string) {
+  return string.split(" ").reverse().join(" ");
+}
+
+reverseMessage3(message);
+
+
+/* NOTES */
+/*
+http://www.sitepoint.com/immutability-javascript/
+
+http://stackoverflow.com/questions/9544182/why-are-strings-immutable-in-many-programming-languages
+
+http://rigaux.org/language-study/various/mutability-and-sharing/
+
+In JavaScript, strings and numbers are immutable by design.
+
+In fact, no string methods change the string they operate on, they all return new strings. The reason is that strings are immutable – they cannot change, we can only ever make new strings.
+
+What is Immutability?
+
+The text-book definition of mutability is liable or subject to change or alteration. In programming, we use the word to mean objects whose state is allowed to change over time. An immutable value is the exact opposite – after it has been created, it can never change.
+
+
+Why strings are immutable and what are the implications of it?
+
+The String type (System.String) stores text values as a sequence of char (System.Char) elements that represent Unicode characters (encoded in UTF-16). Usually one char element stands for one symbol.
+
+When working with text one has to remember that strings in javaScript are immutable! This simply means that once created, strings cannot be modified (without reflection or unsafe code), and the methods that apparently modify a string, really return a new object with the desired value.
+
+Immutability of strings has many advantages, but it can cause problems if programmer forgets that any "change" to the string actually causes creation of a new instance of String class. Although the CLR(Common Language Runtime) treats strings in a special way, they are still a reference type, for which the memory is allocated on the managed heap.
+ */
+
